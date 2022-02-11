8211b52f51cf05322fd3b4adc70b1d3debcaf962
KerolosAdib <keroadib@live.com>
2022-02-11 15:30:33 -0800

    Add Strike System in Commands.java

--- src/main/java/DiscordBot/Commands.java
+++ src/main/java/DiscordBot/Commands.java
@@ -1,30 +1,32 @@
 package DiscordBot;
 
 import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.Role;
-import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.json.simple.JSONObject;
 
+import java.io.File;
 import java.util.HashMap;
 
 public class Commands extends ListenerAdapter {
     HashMap<Member, Integer> strikes;
     public String prefix = "!";
+    private File file;
+    private JSONObject jsonObject;
 
     public Commands()
     {
         super();
-        strikes = new HashMap<>();
-
+        strikes = new HashMap<Member, Integer>();
+        jsonObject = new JSONObject();
     }
 
     public void onMessageReceived(MessageReceivedEvent event)
     {
         String[] args = event.getMessage().getContentRaw().split(" ");
         User user = event.getAuthor();
-        if (args[0].equalsIgnoreCase(prefix + "kick") && event.getMember().hasPermission(Permission.KICK_MEMBERS))
+        if (args[0].equalsIgnoreCase(prefix + "kick") /*&& event.getMember().hasPermission(Permission.KICK_MEMBERS)*/)
         {
             if (event.getMessage().getMentionedMembers().toArray().length == 1)
             {
@@ -59,6 +61,19 @@ public class Commands extends ListenerAdapter {
                 {
                     event.getMessage().reply("3 strikes and " + event.getMessage().getMentionedUsers().get(0).getAsMention() + " is out!").queue();
                     member.kick().queue();
+                }
+            }
+        }
+        else if (args[0].equalsIgnoreCase(prefix + "disconnect") /*&& event.getMember().hasPermission(Permission.ADMINISTRATOR)*/)
+        {
+            if (event.getMessage().getMentionedMembers().toArray().length == 1)
+            {
+                Member member = event.getMessage().getMentionedMembers().get(0);
+                Object[] voiceChannels = event.getGuild().getVoiceChannels().toArray();
+                AudioChannel audioChannel = member.getVoiceState().getChannel();
+                if (audioChannel != null)
+                {
+                    event.getGuild().moveVoiceMember(member, null).queue();
                 }
             }
         }
