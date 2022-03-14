ea026cac0ffa811b635208be38c70ccbcf7a515c
KerolosAdib <keroadib@live.com>
2022-03-14 15:03:04 -0700

    command getstrikes added, EmbedLinkedList, and EmbedNode added

--- src/main/java/DiscordBot/Commands.java
+++ src/main/java/DiscordBot/Commands.java
@@ -1,5 +1,9 @@
 package DiscordBot;
 
+import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.interactions.components.buttons.*;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.*;
@@ -14,28 +18,38 @@ import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.requests.RestAction;
 import net.dv8tion.jda.internal.interactions.CommandDataImpl;
 import org.jetbrains.annotations.NotNull;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 
+import javax.security.auth.callback.Callback;
+import java.awt.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
+import java.util.function.Consumer;
 
 public class Commands extends ListenerAdapter
 {
     HashMap<Guild, HashMap<Member, Integer>> strikes;
+    EmbedLinkedList embedLL;
+    List<EmbedBuilder> ebs;
     public String prefix = "!";
+    private String previousEmbed;
 
     public Commands(JDA jda) throws IOException
     {
         super();
+        previousEmbed = "";
         strikes = new HashMap<Guild, HashMap<Member, Integer>>();
+        ebs = new ArrayList<>();
         jda.updateCommands().queue();
         for (int i = 0; i < jda.getGuilds().size(); i++)
         {
@@ -89,11 +103,7 @@ public class Commands extends ListenerAdapter
                     }
                     strikes.put(guild, temp);
                 }
-                catch (IOException e)
-                {
-                    e.printStackTrace();
-                }
-                catch (ParseException e)
+                catch (IOException | ParseException e)
                 {
                     e.printStackTrace();
                 }
@@ -176,7 +186,8 @@ public class Commands extends ListenerAdapter
                         .addOption(OptionType.USER, "user", "user to strike", true),
                 new CommandDataImpl("roulette", "Chooses a random person to be disconnected"),
                 new CommandDataImpl("kick", "kick a user out")
-                        .addOption(OptionType.USER, "user", "the user to kick out", true)).queue();
+                        .addOption(OptionType.USER, "user", "the user to kick out", true),
+                new CommandDataImpl("getstrikes", "grabs strikes")).queue();
     }
 
 
@@ -185,31 +196,30 @@ public class Commands extends ListenerAdapter
     {
         super.onGuildMemberJoin(event);
         JSONParser jsonParser = new JSONParser();
-        try (FileReader reader = new FileReader("strikes.json"))
+        try (FileReader reader = new FileReader(event.getGuild().getId() + ".json"))
         {
             Object obj = jsonParser.parse(reader);
-            JSONObject guildList = (JSONObject) obj;
-            for (int i = 0; i < guildList.size(); i++)
+            JSONObject memberList = (JSONObject) obj;
+            if (memberList.containsKey(event.getMember().getId()))
             {
-                if (guildList.keySet().toArray()[i].equals(event.getGuild().getId()))
+                strikes.get(event.getGuild()).put(event.getMember(), Math.toIntExact((Long) memberList.get(event.getMember().getId())));
+            }
+            else
+            {
+                strikes.get(event.getGuild()).put(event.getMember(), 0);
+                memberList.put(event.getMember().getId(), 0);
+                try (FileWriter fileWriter = new FileWriter(event.getGuild().getId() + ".json"))
                 {
-                    JSONObject memberList = (JSONObject) guildList.get(guildList.keySet().toArray()[i]);
-                    if (memberList.containsKey(event.getMember().getId()))
-                    {
-                        strikes.get(event.getGuild()).put(event.getMember(), Math.toIntExact((Long) memberList.get(event.getMember().getId())));
-                    }
-                    else
-                    {
-                        strikes.get(event.getGuild()).put(event.getMember(), 0);
-                    }
+                    fileWriter.write(memberList.toJSONString());
+                    fileWriter.flush();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
                 }
             }
         }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        catch (ParseException e)
+        catch (IOException | ParseException e)
         {
             e.printStackTrace();
         }
@@ -298,12 +308,14 @@ public class Commands extends ListenerAdapter
             if (strikes.get(event.getGuild()).get(member) < 3)
             {
                 event.reply("User: " + member.getAsMention() + " has " + strikes.get(event.getGuild()).get(member) + " strike(s)!").queue();
+                addStrikeToJSON(event.getGuild(), member, 1);
             }
-            if (strikes.get(event.getGuild()).get(member) >= 3)
+            else
             {
                 event.reply("3 strikes and " + member.getAsMention() + " is out!").queue();
                 member.kick().queue();
             }
+
 
         }
         else if (event.getName().equals("roulette"))
@@ -327,19 +339,109 @@ public class Commands extends ListenerAdapter
             Member member = event.getOption("user").getAsMember();
             member.kick().queue();
         }
+        else if (event.getName().equals("getstrikes"))
+        {
+            embedLL = new EmbedLinkedList();
+            Guild guild = event.getGuild();
+            List<Member> members = guild.getMembers();
+            event.deferReply(true).queue();
+            int pages = (int) Math.ceil(guild.getMemberCount() / 10.0);
+            List<Button> buttons = new ArrayList<>();
+            for (int i = 1; i <= pages; i++)
+            {
+                EmbedBuilder eb = new EmbedBuilder();
+                eb.setTitle("Strikes");
+                eb.setDescription("Shows the number of strikes each person has");
+                eb.setFooter("Page: " + i);
+                eb.setColor(Color.RED);
+                int size = strikes.get(guild).size();
+
+                for (int j = 0; j < 10 && ((i - 1) * 10 + j) < size; j++)
+                {
+                    String value = members.get((i - 1) * 10 + j).getAsMention() + ": ";
+                    int numberOfStrikes = strikes.get(guild).get(members.get((i - 1) * 10 + j));
+                    if (numberOfStrikes == 0)
+                        value += "Good job, no strikes!";
+                    else if (numberOfStrikes == 1)
+                        value += "❌";
+                    else if (numberOfStrikes == 2)
+                        value += "❌❌";
+                    else
+                        value += "❌❌❌";
+                    eb.addField("", value, false);
+                }
+                embedLL.addBack(eb);
+            }
+
+            buttons.add(Button.primary("first", Emoji.fromUnicode("⏪")));
+            buttons.add(Button.primary("last_page", Emoji.fromUnicode("◀")));
+            buttons.add(Button.primary("next_page", Emoji.fromUnicode("▶")));
+            buttons.add(Button.primary("last", Emoji.fromUnicode("⏩")));
+
+            embedLL.setCurrent(embedLL.getHeadBuilder());
+            RestAction<Message> ra = event.getHook().sendMessageEmbeds(embedLL.getCurrentBuilder().build()).addActionRow(buttons);
+            Message message = ra.complete();
+
+
+            if (!previousEmbed.equals(""))
+                event.getMessageChannel().deleteMessageById(previousEmbed).queue();
+            previousEmbed = message.getId();
+
+
+        }
     }
 
-/*    public void addStrikeToJSON(Member member, int strikes)
+    @Override
+    public void onButtonInteraction(@NotNull ButtonInteractionEvent event)
     {
-        try (FileReader fileReader = new FileReader("strikes"))
+        List<Button> buttons = new ArrayList<>();
+        Guild guild = event.getGuild();
+        List<Member> members = guild.getMembers();
+        int pages = (int) Math.ceil(guild.getMemberCount() / 10.0);
+        EmbedBuilder eb = new EmbedBuilder();
+        buttons.add(Button.primary("first", Emoji.fromUnicode("⏪")));
+        buttons.add(Button.primary("last_page", Emoji.fromUnicode("◀")));
+        buttons.add(Button.primary("next_page", Emoji.fromUnicode("▶")));
+        buttons.add(Button.primary("last", Emoji.fromUnicode("⏩")));
+        switch (event.getComponentId())
         {
+            case "first":
+                embedLL.setCurrent(embedLL.getHeadBuilder());
+                break;
+            case "last_page":
+                embedLL.decrementCurrent();
+                break;
+            case "next_page":
+                embedLL.incrementCurrent();
+                break;
+            case "last":
+                embedLL.setCurrent(embedLL.getTailBuilder());
+                break;
+        }
+        event.editMessageEmbeds(embedLL.getCurrentBuilder().build()).setActionRow(buttons).queue();
+    }
+
+    public void addStrikeToJSON(Guild guild, Member member, int strikes)
+    {
+        JSONParser jsonParser = new JSONParser();
+        try (FileReader reader = new FileReader(guild.getId() + ".json"))
+        {
+            Object obj = jsonParser.parse(reader);
+            JSONObject memberList = (JSONObject) obj;
+            memberList.put(member.getId(), (Long) memberList.get(member.getId()) + strikes);
+            FileWriter fileWriter = new FileWriter(guild.getId() + ".json");
+
+            fileWriter.write(memberList.toJSONString());
+            fileWriter.flush();
+
 
         }
-        catch (IOException e)
+        catch (IOException | ParseException e)
         {
-
+            e.printStackTrace();
         }
-    }*/
+    }
+
 
 
     /*    @Override
---
+++ src/main/java/DiscordBot/EmbedLinkedList.java
@@ -0,0 +1,151 @@
+package DiscordBot;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+
+
+public class EmbedLinkedList
+{
+    private EmbedNode head;
+    private EmbedNode tail;
+    private EmbedNode current;
+    private int size = 0;
+
+    public void addBack(EmbedBuilder info) {
+        size++;
+        if (head == null) {
+            head = new EmbedNode(info, null, null);
+            tail = head;
+        } else {
+            EmbedNode node = new EmbedNode(info, null, tail);
+            this.tail.next = node;
+            this.tail = node;
+        }
+        tail.next = head;
+        head.prev = tail;
+    }
+
+    public void addFront(EmbedBuilder info) {
+        size++;
+        if (head == null) {
+            head = new EmbedNode(info, null, null);
+            tail = head;
+        } else {
+            EmbedNode node = new EmbedNode(info, head, null);
+            this.head.prev = node;
+            this.head = node;
+        }
+        tail.next = head;
+        head.prev = tail;
+    }
+
+    public EmbedBuilder removeBack() {
+        EmbedBuilder result = null;
+        if (head != null) {
+            size--;
+            result = tail.info;
+            if (tail.prev != null) {
+                tail.prev.next = null;
+                tail = tail.prev;
+                tail.next = head;
+                head.prev = tail;
+            } else {
+                head = null;
+                tail = null;
+            }
+        }
+        return result;
+    }
+
+    public EmbedBuilder removeFront() {
+        EmbedBuilder result = null;
+        if (head != null) {
+            size--;
+            result = head.info;
+            if (head.next != null) {
+                head.next.prev = null;
+                head = head.next;
+                tail.next = head;
+                head.prev = tail;
+            } else {
+                head = null;
+                tail = null;
+            }
+        }
+        return result;
+    }
+
+    public EmbedBuilder getHeadBuilder() {
+        return head.info;
+    }
+
+    public EmbedBuilder getTailBuilder() {
+        return tail.info;
+    }
+
+    public void setCurrent(EmbedBuilder eb)
+    {
+        EmbedNode it = head;
+        while (it.info != eb)
+        {
+            it = it.next;
+        }
+        current = it;
+    }
+
+    public void decrementCurrent()
+    {
+        current = current.prev;
+    }
+
+    public void incrementCurrent()
+    {
+        current = current.next;
+    }
+
+
+    public EmbedBuilder getCurrentBuilder() { return current.info; }
+
+    public class EmbedNode
+    {
+        private EmbedNode next;
+        private EmbedNode prev;
+        private EmbedBuilder info;
+
+        public EmbedNode(EmbedBuilder info, EmbedNode next, EmbedNode prev)
+        {
+            this.next = next;
+            this.prev = prev;
+            this.info = info;
+        }
+
+        public void setInfo(EmbedBuilder info)
+        {
+            this.info = info;
+        }
+
+        public void setNext(EmbedNode next)
+        {
+            this.next = next;
+        }
+
+        public void setPrev(EmbedNode prev)
+        {
+            this.prev = prev;
+        }
+
+        public EmbedNode getNext()
+        {
+            return next;
+        }
+
+        public EmbedNode getPrev()
+        {
+            return prev;
+        }
+
+        public EmbedBuilder getInfo()
+        {
+            return info;
+        }
+    }
+}
